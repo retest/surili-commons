62706846dd0333be343eec345390cd667b6205a7
Daniel Kraus <daniel.kraus@retest.de>
2019-06-11 12:51:29 +0200

    Apply formatter

--- src/main/java/de/retest/surili/commons/test/TestUtil.java
+++ src/main/java/de/retest/surili/commons/test/TestUtil.java
@@ -14,12 +14,13 @@ import de.retest.recheck.ui.image.Screenshot;
 
 public final class TestUtil {
 
-	private TestUtil() { }
+	private TestUtil() {}
 
 	/**
 	 * Creates a new SutState which is equal to another SutState if they contain equal collections of root elements.
 	 *
-	 * @param rootElements The collection of root elements of the SutState.
+	 * @param rootElements
+	 *            The collection of root elements of the SutState.
 	 * @return A new SutState.
 	 */
 	public static SutState createSutState( final RootElement... rootElements ) {
@@ -35,15 +36,15 @@ public final class TestUtil {
 	 * Creates a new root element identified by a user-defined ID. Root elements with the same ID and number of children
 	 * should be equal.
 	 *
-	 * @param id               The ID which uniquely identifies the root element as long as the number of children is
-	 *                         the same.
-	 * @param numberOfChildren The number of contained components of the root element.
+	 * @param id
+	 *            The ID which uniquely identifies the root element as long as the number of children is the same.
+	 * @param numberOfChildren
+	 *            The number of contained components of the root element.
 	 * @return A new root element.
 	 */
 	public static RootElement getRootElement( final String id, final int numberOfChildren ) {
 		final RootElement rootElement = new RootElement( "retestId", getIdentifyingAttributes( id ), new Attributes(),
-				new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ), "screen0",
-				0,
+				new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ), "screen0", 0,
 				"My Window" );
 		if ( numberOfChildren > 0 ) {
 			final List<Element> children = new ArrayList<>();
@@ -58,7 +59,7 @@ public final class TestUtil {
 	}
 
 	public static Element getElement( final Element parent, final int index ) {
-		return Element.create( "retestId", parent, getIdentifyingAttributes( "child" + index ),
-				new Attributes(), new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ) );
+		return Element.create( "retestId", parent, getIdentifyingAttributes( "child" + index ), new Attributes(),
+				new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ) );
 	}
 }
