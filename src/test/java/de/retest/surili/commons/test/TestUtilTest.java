90bd774014bb27939432e11bde4de891f7da2738
Tamino Dauth <tamino@cdauth.eu>
2019-06-12 11:13:55 +0200

    Adapt unit tests

--- src/test/java/de/retest/surili/commons/test/TestUtilTest.java
+++ src/test/java/de/retest/surili/commons/test/TestUtilTest.java
@@ -1,7 +1,6 @@
 package de.retest.surili.commons.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
 import java.util.List;
@@ -9,57 +8,40 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import de.retest.recheck.ui.descriptors.Element;
-import de.retest.recheck.ui.descriptors.IdentifyingAttributes;
 import de.retest.recheck.ui.descriptors.RootElement;
 import de.retest.recheck.ui.descriptors.SutState;
 
 class TestUtilTest {
 
 	@Test
-	void created_sut_state_should_contain_given_root_elements() throws Exception {
-		final RootElement re0 = mock( RootElement.class );
-		final RootElement re1 = mock( RootElement.class );
-
-		final SutState sutState = TestUtil.createSutState( re0, re1 );
-
-		assertThat( sutState.getRootElements() ).containsExactly( re0, re1 );
-	}
-
-	@Test
-	void created_identifying_attributes_should_TODO() throws Exception {
-		final String id = "foo";
-
-		final IdentifyingAttributes identifyingAttributes = TestUtil.getIdentifyingAttributes( id );
-
-		// TODO Add assertion and adapt test case name.
-		fail();
-	}
-
-	@Test
-	void created_root_element_should_TODO() throws Exception {
-		final String id = "foo";
+	void created_sut_states_elements_should_be_equal() throws Exception {
 		final int numberOfChildren = 3;
 
-		final RootElement rootElement = TestUtil.getRootElement( id, numberOfChildren );
+		final RootElement rootElement0 = TestUtil.getRootElement( "foo", numberOfChildren );
+		final RootElement rootElement1 = TestUtil.getRootElement( "foo", numberOfChildren );
 
 		final List<Element> containedElements = rootElement.getContainedElements();
 		assertThat( containedElements ).hasSize( numberOfChildren );
 
-		// TODO Add assertion and adapt test case name.
-		fail();
+		final SutState sutState0 = TestUtil.createSutState( rootElement0 );
+		final SutState sutState1 = TestUtil.createSutState( rootElement1 );
+
+		assertThat( sutState0, equalTo( sutState1 ) );
 	}
 
 	@Test
-	void created_element_should_TODO() throws Exception {
-		final Element parent = mock( Element.class );
-		final int index = 1;
+	void created_sut_states_should_not_be_equal() throws Exception {
+		final int numberOfChildren = 3;
 
-		final Element element = TestUtil.getElement( parent, index );
+		final RootElement rootElement0 = TestUtil.getRootElement( "foo", numberOfChildren );
+		final RootElement rootElement1 = TestUtil.getRootElement( "bar", numberOfChildren );
 
-		assertThat( element.getParent() ).isEqualTo( parent );
+		final List<Element> containedElements = rootElement.getContainedElements();
+		assertThat( containedElements ).hasSize( numberOfChildren );
 
-		// TODO Add assertion and adapt test case name.
-		fail();
+		final SutState sutState0 = TestUtil.createSutState( rootElement0 );
+		final SutState sutState1 = TestUtil.createSutState( rootElement1 );
+
+		assertThat( sutState0, not( equalTo( sutState1 ) ) );
 	}
-
 }
