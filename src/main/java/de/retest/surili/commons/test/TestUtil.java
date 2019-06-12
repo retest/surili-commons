9f8b4ae45dec1abce3e86d9ff3fa77b472821502
Tamino Dauth <tamino@cdauth.eu>
2019-06-12 12:09:14 +0200

    Rename parameter

--- src/main/java/de/retest/surili/commons/test/TestUtil.java
+++ src/main/java/de/retest/surili/commons/test/TestUtil.java
@@ -20,7 +20,7 @@ public final class TestUtil {
 	 * Creates a new SutState which is equal to another SutState if they contain equal collections of root elements.
 	 *
 	 * @param rootElements
-	 *            The collection of root elements of the SutState.
+	 * 		The collection of root elements of the SutState.
 	 * @return A new SutState.
 	 */
 	public static SutState createSutState( final RootElement... rootElements ) {
@@ -31,16 +31,19 @@ public final class TestUtil {
 	 * Creates a new root element identified by a user-defined ID. Root elements with the same ID and number of children
 	 * should be equal.
 	 *
-	 * @param id
-	 *            The ID which uniquely identifies the root element as long as the number of children is the same.
+	 * @param identifyingAttributesValue
+	 * 		Used as an identifying attributes value to ensure equality as long as the number of children is the same.
 	 * @param numberOfChildren
-	 *            The number of contained components of the root element.
+	 * 		The number of contained components of the root element.
 	 * @return A new root element.
 	 */
-	public static RootElement getRootElement( final String id, final int numberOfChildren ) {
-		final RootElement rootElement = new RootElement( "retestId", getIdentifyingAttributes( id ), new Attributes(),
-				new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ), "screen0", 0,
-				"My Window" );
+	public static RootElement getRootElement( final String identifyingAttributesValue, final int numberOfChildren ) {
+		final RootElement rootElement =
+				new RootElement( "retestId", getIdentifyingAttributes( identifyingAttributesValue ),
+						new Attributes(),
+						new Screenshot( "prefix", new byte[] { 1, 2, 3 }, Screenshot.ImageType.PNG ), "screen0",
+						0,
+						"My Window" );
 		if ( numberOfChildren > 0 ) {
 			final List<Element> children = new ArrayList<>();
 
@@ -53,9 +56,10 @@ public final class TestUtil {
 		return rootElement;
 	}
 
-	private static IdentifyingAttributes getIdentifyingAttributes( final String id ) {
-		return new IdentifyingAttributes( Arrays.asList( new StringAttribute( "a", id ), new StringAttribute( "b", id ),
-				new StringAttribute( "c", id ) ) );
+	private static IdentifyingAttributes getIdentifyingAttributes( final String identifyingAttributesValue ) {
+		return new IdentifyingAttributes( Arrays.asList( new StringAttribute( "a", identifyingAttributesValue ),
+				new StringAttribute( "b", identifyingAttributesValue ),
+				new StringAttribute( "c", identifyingAttributesValue ) ) );
 	}
 
 	private static Element getElement( final Element parent, final int index ) {
