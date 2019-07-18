5502e33a5f7f728977c4ea9df45bf93d3edadce7
Daniel Kraus <daniel.kraus@retest.de>
2019-07-18 14:50:58 +0200

    Add basic Javadoc

--- src/main/java/de/retest/surili/commons/ga/MultiObjectiveFitness.java
+++ src/main/java/de/retest/surili/commons/ga/MultiObjectiveFitness.java
@@ -5,6 +5,10 @@ import java.util.List;
 import lombok.NonNull;
 import lombok.Value;
 
+/**
+ * {@link Fitness} list to be optimized as part of a
+ * <a href="https://en.wikipedia.org/wiki/Multi-objective_optimization">multi-objective optimization problem</a>.
+ */
 @Value
 public class MultiObjectiveFitness {
 	@NonNull
