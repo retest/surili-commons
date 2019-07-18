482c6336062c12c455f98c7ef760e440e1aed9bd
Daniel Kraus <daniel.kraus@retest.de>
2019-07-18 15:00:01 +0200

    Add more Javadoc

--- src/main/java/de/retest/surili/commons/core/TestSuite.java
+++ src/main/java/de/retest/surili/commons/core/TestSuite.java
@@ -3,12 +3,24 @@ package de.retest.surili.commons.core;
 import java.io.Serializable;
 import java.util.Set;
 
+/**
+ * A test suite with a set (i.e. unordered) of {@link TestCase}s.
+ */
 public interface TestSuite extends Serializable {
 
+	/**
+	 * @return The display name of this test suite.
+	 */
 	String getName();
 
+	/**
+	 * @return The set of test cases of this test suite.
+	 */
 	Set<TestCase> getTestCases();
 
+	/**
+	 * @return The number of test cases of this test suite.
+	 */
 	int getSize();
 
 }
