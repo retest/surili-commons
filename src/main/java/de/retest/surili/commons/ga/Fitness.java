8583b9ee063538475e6a9586922e20f48859c37a
Daniel Kraus <daniel.kraus@retest.de>
2019-10-29 19:39:29 +0100

    Implement `Comparable`
    Also needed by Jenetics.

--- src/main/java/de/retest/surili/commons/ga/Fitness.java
+++ src/main/java/de/retest/surili/commons/ga/Fitness.java
@@ -4,12 +4,17 @@ package de.retest.surili.commons.ga;
  * Generic interface for representing fitness, be it variants of code coverage (e.g. statement coverage or branch
  * coverage) or something entirely different (e.g. found bugs or test length).
  */
-public interface Fitness {
+public interface Fitness extends Comparable<Fitness> {
 
 	/**
 	 * @return Fitness measured in the corresponding problem domain's unit. As fitness is maximized, a higher value
 	 *         indicates a better fitness.
 	 */
 	double getFitness();
+
+	@Override
+	default int compareTo( final Fitness other ) {
+		return Double.compare( getFitness(), other.getFitness() );
+	}
 
 }
