873705f4c6242034a6fbafb8b839cfb144f4eb4a
Daniel Kraus <daniel.kraus@retest.de>
2019-11-11 17:26:06 +0100

    Add util to create random booleans

--- src/main/java/de/retest/surili/commons/util/RandomUtil.java
+++ src/main/java/de/retest/surili/commons/util/RandomUtil.java
@@ -1,5 +1,7 @@
 package de.retest.surili.commons.util;
 
+import java.util.Random;
+
 public class RandomUtil {
 
 	public static double requireProbability( final double probability ) {
@@ -7,6 +9,10 @@ public class RandomUtil {
 			throw new IllegalArgumentException( "The given probability is not in the range [0, 1]: " + probability );
 		}
 		return probability;
+	}
+
+	public static boolean randomBoolean( final Random random, final double probability ) {
+		return random.nextDouble() <= requireProbability( probability );
 	}
 
 }
--- src/test/java/de/retest/surili/commons/util/RandomUtilTest.java
+++ src/test/java/de/retest/surili/commons/util/RandomUtilTest.java
@@ -3,6 +3,9 @@ package de.retest.surili.commons.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -20,6 +23,15 @@ class RandomUtilTest {
 		assertThatThrownBy( () -> RandomUtil.requireProbability( probability ) ) //
 				.isInstanceOf( IllegalArgumentException.class ) //
 				.hasMessage( "The given probability is not in the range [0, 1]: " + probability );
+	}
+
+	@Test
+	void should_create_random_booleans_with_given_success_probability() throws Exception {
+		final Random random = new Random( 0L );
+		assertThat( RandomUtil.randomBoolean( random, 0.0 ) ).isFalse();
+		assertThat( RandomUtil.randomBoolean( random, 1.0 ) ).isTrue();
+		assertThat( RandomUtil.randomBoolean( random, 0.4 ) ).isFalse();
+		assertThat( RandomUtil.randomBoolean( random, 0.6 ) ).isTrue();
 	}
 
 }
