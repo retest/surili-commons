08d95f6436fa59a03a6c2bab1cc5e0ea20b61361
Daniel Kraus <daniel.kraus@retest.de>
2019-11-29 10:52:02 +0100

    Add interface previously defined in surili-web

---
+++ src/main/java/de/retest/surili/commons/actionexecution/ActionExecutor.java
@@ -0,0 +1,12 @@
+package de.retest.surili.commons.actionexecution;
+
+import de.retest.surili.commons.actions.Action;
+
+/**
+ * Executes one or more given {@link Action}s.
+ */
+public interface ActionExecutor {
+
+	void execute();
+
+}
