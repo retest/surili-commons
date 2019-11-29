cc1b2f25a20c744f0f2a5ebdf03ad865a0835880
Daniel Kraus <daniel.kraus@retest.de>
2019-11-29 10:55:17 +0100

    Reduce verbosity

--- src/main/java/de/retest/surili/commons/ga/Bugs.java
+++ src/main/java/de/retest/surili/commons/ga/Bugs.java
@@ -3,6 +3,6 @@ package de.retest.surili.commons.ga;
 /**
  * Marker interface for any form of found bugs (e.g. HTTP/backend errors, JS/frontend errors, dead links).
  */
-public interface BugFitness extends Fitness {
+public interface Bugs extends Fitness {
 
 }
